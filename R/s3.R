51f7e99339088a24766a5e1336bb8b98d79317b7
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-09-02 01:27:04 +0200

    safe download of files from S3

--- R/s3.R
+++ R/s3.R
@@ -1,3 +1,6 @@
+#' Raw access to boto3.resource('s3')
+#' @export
+#' @references \url{https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#service-resource}
 s3 <- NULL
 
 
@@ -9,6 +12,20 @@ assert_s3 <- function() {
     if (is.null(s3) || attr(s3, 'pid') != Sys.getpid()) {
         utils::assignInMyNamespace('s3', structure(botor$resource('s3'), pid = Sys.getpid()))
     }
+}
+
+
+#' Extract bucket name and key from S3 path
+#' @param path S3 path starting with \code{s3://}, bucket name and object key
+#' @return list
+#' @export
+s3_extract_path <- function(path) {
+    assert_s3_path(path)
+    path <- sub('^s3://', '', path)
+    list(
+        bucket_name = sub('/.*$', '', path),
+        key = sub('^[a-z0-9][a-z0-9\\.-]+[a-z0-9]/', '', path)
+    )
 }
 
 
@@ -24,4 +41,30 @@ s3_list_buckets <- function(simplify = TRUE) {
         buckets <- sapply(buckets, `[[`, 'name')
     }
     buckets
+}
+
+
+#' Download a file from S3
+#' @param object string, remote S3 path of file to download, should start with 's3://'
+#' @param file string, location of local file
+#' @param overwrite boolean, overwrite local file if exists
+#' @export
+#' @importFrom checkmate assert_string assert_directory_exists assert_flag
+#' @importFrom aws.s3 save_object
+#' @return invisibly \code{file}
+#' @examples \dontrun{
+#' s3_download('s3://botor/example-data/mtcars.csv', tempfile())
+#' }
+s3_download <- function(object, file, force = TRUE) {
+    assert_string(file)
+    assert_directory_exists(dirname(file))
+    if (force == FALSE & file.exists(file)) {
+        stop(paste(file, 'already exists'))
+    }
+    assert_s3_path(object)
+    assert_flag(force)
+    assert_s3()
+    s3object <- s3$Object(bucket_name = s3_extract_path(object)$bucket_name, key = s3_extract_path(object)$key)
+    s3object$download_file(file)
+    invisible(file)
 }
