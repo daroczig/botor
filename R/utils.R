d4cdddee904d822a2f773569bfc8c038d3bd5c73
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-09-02 14:00:30 +0200

    more fun with scoping

--- R/utils.R
+++ R/utils.R
@@ -2,11 +2,24 @@
 #' @param expression R expression
 #' @return error
 #' @keywords internal
+#' @examples \dontrun{
+#' trypy(botor$resource('foobar'))
+#' trypy(sum(1:2))
+#' trypy(sum(1:1foo))
+#' }
 trypy <- function(expression) {
+    py_clear_last_error()
     tryCatch(
-        eval.parent(expression),
-        error = function(e) {
+    eval.parent(expression),
+    error = function(e) {
+        e$call <- sys.calls()[[1]]
+        if (sys.nframe() > 5) {
             e$call <- sys.calls()[[sys.nframe() - 5]]
-            stop(e)
-        })
+        }
+        pe <- py_last_error()
+        if (!is.null(pe)) {
+            e$message <- paste0('Python `', pe$type, '`: ', pe$value)
+        }
+        stop(e)
+    })
 }
