ff5edae48124daf888a28fa2a4baa2a907dc900a
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-10-25 01:25:20 +0200

    add kms_decrypt

--- R/kms.R
+++ R/kms.R
@@ -21,13 +21,29 @@ kms <- function() {
 #' @param key the KMS customer master key identifier as a fully specified Amazon Resource Name (eg \code{arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012}) or an alias with the \code{alias/} prefix (eg \code{alias/foobar})
 #' @param text max 4096 bytes long string, eg an RSA key, a database password, or other sensitive customer information
 #' @param simplify returns Base64-encoded text instead of raw list
-#' @return list or string
+#' @return string or \code{list}
 #' @export
+#' @seealso kms_decrypt
 kms_encrypt <- function(key, text, simplify = TRUE) {
     res <- kms()$encrypt(KeyId = key, Plaintext = charToRaw(text))
     if (simplify == TRUE) {
         res <- res$CiphertextBlob
         res <- base64_enc(res)
+    }
+    res
+}
+
+
+#' Decrypt cipher into plain text via KMS
+#' @param cipher Base64-encoded ciphertext
+#' @param simplify returns decrypted plain-text instead of raw list
+#' @return decrypted text as string or \code{list}
+#' @export
+#' @seealso kms_encrypt
+kms_decrypt <- function(cipher, simplify = TRUE) {
+    res <- kms()$decrypt(CiphertextBlob = base64_dec(cipher))
+    if (simplify == TRUE) {
+        res <- res$Plaintext
     }
     res
 }
