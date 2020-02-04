1ca0e604af94c01ed54ca61e19957b59fddc2884
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2020-02-04 07:34:51 +0100

    fix indent

--- R/utils.R
+++ R/utils.R
@@ -11,18 +11,18 @@
 trypy <- function(expression) {
     py_clear_last_error()
     tryCatch(
-    eval.parent(expression),
-    error = function(e) {
-        e$call <- sys.calls()[[1]]
-        if (sys.nframe() > 5) {
-            e$call <- sys.calls()[[sys.nframe() - 5]]
-        }
-        pe <- py_last_error()
-        if (!is.null(pe)) {
-            e$message <- paste0('Python `', pe$type, '`: ', pe$value)
-        }
-        stop(e)
-    })
+        eval.parent(expression),
+        error = function(e) {
+            e$call <- sys.calls()[[1]]
+            if (sys.nframe() > 5) {
+                e$call <- sys.calls()[[sys.nframe() - 5]]
+            }
+            pe <- py_last_error()
+            if (!is.null(pe)) {
+                e$message <- paste0('Python `', pe$type, '`: ', pe$value)
+            }
+            stop(e)
+        })
 }
 
 
