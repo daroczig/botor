429b722aa2902ebeb3dec23f8ef43777738f5e42
amy17519 <amy17519@gmail.com>
2020-05-19 21:54:03 +0000

    update sm_get_secrets and docs

--- R/ssm.R
+++ R/ssm.R
@@ -61,26 +61,33 @@ ssm_get_secrets <- function(path, key = NULL, decrypt = TRUE) {
 
 #' Read AWS System Manager's Secrets Manager via Secret Manager
 #' @param path name/path of the key to be read
-#' @param key keyID
-#' @importFrom checkmate assert_string
+#' @param keyinpath logical. Default TRUE
+#' @param output_format return format. Either as raw_string or value
+#' @importFrom checkmate assert_string assert_logical
 #' @importFrom jsonlite fromJSON
 #' @return (optionally decrypted) value
 #' @export
-sm_get_secrets <- function(path, key = NULL) {
-    # Format: path = your/secretname/key 
-    # or 
-    # path = your/seretname, key = key
+sm_get_secrets <- function(path, keyinpath = TRUE, output_format = c("raw_string", "value")) {
+    match.arg(output_format)
+    assert_logical(keyinpath)
     
-    if (is.null(key)) {
+    if (keyinpath) {
         parts <- unlist(strsplit(path, "/"))
         key <- tail(parts, 1)
         path <- paste(parts[1:length(parts) - 1], collapse = "/")
     }
-
-    log_trace("Looking up keyID %s from SecretId %s in AWS Secrets Manager", key, path)
-    
+        
     resp <- trypy(sm()$get_secret_value(
-                SecretId = path))$SecretString
-    fromJSON(resp)[[key]]
-}
+        SecretId = path))$SecretString
+    
+    log_trace("Looking up SecretId %s in AWS Secrets Manager", path)
+    
+    if (output_format == "raw_string") {
+        resp
+    } else if (output_format == "value" & !is.null(key)) {
+        fromJSON(resp)[[key]]
+    } else {
+        stop("Invalid output format or missing key!")
+    }
 
+}
