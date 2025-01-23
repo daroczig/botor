e2aabcb2003796442e8a228c5dce082127b20a8a
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2025-01-24 00:45:04 +0100

    disable live S3 queries in unit tests for public objects (404)

--- inst/tests/testthat/test-load.R
+++ inst/tests/testthat/test-load.R
@@ -3,7 +3,7 @@ library(testthat)
 
 context('s3')
 
-test_that('load data from s3', {
-    expect_true(s3_exists('s3://botor/example-data/mtcars.csv'))
-    expect_error(s3_read('s3://botor/example-data/mtcars.csv', read.csv), NA)
-})
+## test_that('load data from s3', {
+##     expect_true(s3_exists('s3://botor/example-data/mtcars.csv'))
+##     expect_error(s3_read('s3://botor/example-data/mtcars.csv', read.csv), NA)
+## })
